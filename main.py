97595d074ac8b9f6ed6344e29d321cdcb3111010
PabloPizarro8 <l20550431@chihuahua2.tecnm.mx>
2024-04-29 22:12:36 -0600

    git commit -m "Add main.py"

---
+++ main.py
@@ -0,0 +1,7 @@
+# Proyecto Juego con camara web
+# Materia: Inteligencia Artificial
+# Profesor: Arturo Legarda Saens
+# Alumnos:  Jonathan Duran Mendoza
+#           Pablo Pizarro Chalup    20550431
+
+# Descripcion: Este programa es un juego que hace uso de la camara web para detectar la posicion de la mano del jugador y asi poder mover un objeto en la pantalla.
