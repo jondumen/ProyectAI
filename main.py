d092b82401bced37267390a935a75343e5d4ff55
jondumen <84062001+jondumen@users.noreply.github.com>
2024-04-29 23:20:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Proyecto Juego con camara web
 # Materia: Inteligencia Artificial
 # Profesor: Arturo Legarda Saens
-# Alumnos:  Jonathan Duran Mendoza
+# Alumnos:  Jonathan Duran Mendoza  20550401
 #           Pablo Pizarro Chalup    20550431
 
 # Descripcion: Este programa es un juego que hace uso de la camara web para detectar la posicion de la mano del jugador y asi poder mover un objeto en la pantalla.
